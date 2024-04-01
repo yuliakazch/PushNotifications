34ff267ac5c5145395384b1bc8592f68f2ffc3d1
yuliakazch <yuliakazachok@gmail.com>
2024-04-01 17:07:11 +0700

    Add changes for getting fcm token

--- app/src/main/java/com/yuliakazachok/pushnotifications/MainActivity.kt
+++ app/src/main/java/com/yuliakazachok/pushnotifications/MainActivity.kt
@@ -1,13 +1,25 @@
 package com.yuliakazachok.pushnotifications
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import com.google.firebase.messaging.FirebaseMessaging
 import com.yuliakazachok.pushnotifications.ui.theme.PushNotificationsTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        FirebaseMessaging.getInstance().token.addOnCompleteListener { task ->
+            if (!task.isSuccessful) {
+                return@addOnCompleteListener
+            }
+
+            val token = task.result
+            Log.d("firebase", "token = $token")
+        }
+
         setContent {
             PushNotificationsTheme {
             }
